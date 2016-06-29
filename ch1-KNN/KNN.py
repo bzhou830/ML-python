0cf55cd098335cd4a1cf86dfd8d2c8e2b7d60037
robin <chou_robin@hotmail.com>
2016-06-29 17:35:08 +0800

    modify

--- ch1-KNN/KNN.py
+++ ch1-KNN/KNN.py
@@ -4,17 +4,24 @@
 from numpy import *
 import operator
 
+
 def createDataSet():
     group = array([[1.0,1.1],[1.0,1.0],[0,0],[0,0.1]])
     labels = ['A','A','B','B']
     return group, labels
 
 
+'''
+inx         输入向量
+dataSet     训练数据集
+labels      训练数据集标签
+k           最临近数目
+'''
 def classify0(inx,dataSet,labels,k):
-    dataSetSize=dataSet.shape[0] #样本集的个数
-    diffMat=tile(inx,(dataSetSize,1)) - dataSet
-    sqDiffMat = diffMat**2
-    sqDistances=sqDiffMat.sum(axis=1)
+    dataSetSize=dataSet.shape[0]                                #样本集的个数
+    diffMat=tile(inx,(dataSetSize,1)) - dataSet                 #矩阵之差
+    sqDiffMat = diffMat**2                                      #矩阵之差的平方
+    sqDistances=sqDiffMat.sum(axis=1)                           
     distances=sqDistances**0.5
     sortedDistIndicies = distances.argsort()
     classCount={}
@@ -26,11 +33,11 @@ def classify0(inx,dataSet,labels,k):
 
 
 def file2matrix(filename):
-    fr = open(filename)                 #打开文件
-    arrayOfLines = fr.readlines()       #读取文件行数
-    numberOfLines = len(arrayOfLines)   #文件行数
-    returnMat=zeros((numberOfLines,3))  #矩阵
-    classLabelVector = []               #标签
+    fr = open(filename)                                             #打开文件
+    arrayOfLines = fr.readlines()                                   #读取文件行数
+    numberOfLines = len(arrayOfLines)                               #文件行数
+    returnMat=zeros((numberOfLines,3))                              #矩阵
+    classLabelVector = []                                           #标签
     index = 0                           
     for line in arrayOfLines:           
         line = line.strip() 
@@ -53,13 +60,13 @@ def autoNorm(dataSet):
 
 
 def datingClassTest():
-    hoRatio = 0.20
-    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')
-    normMat, ranges, minVals = autoNorm(datingDataMat)
-    m = normMat.shape[0]
-    numTestVecs = int(m*hoRatio)
+    hoRatio = 0.20                                                  #测试数据集所占比例
+    datingDataMat,datingLabels = file2matrix('datingTestSet2.txt')  #加载数据
+    normMat, ranges, minVals = autoNorm(datingDataMat)              #归一化数据
+    m = normMat.shape[0]                                            
+    numTestVecs = int(m*hoRatio)                                    #测试数数量
     errorcount = 0.0
-    for i in range(numTestVecs):
+    for i in range(numTestVecs):                                    #
         classifierResult = classify0(normMat[i,:],normMat[numTestVecs:m,:],datingLabels[numTestVecs:m],3)
         print "Rt is %d, Real is %d" % (classifierResult,datingLabels[i])
         if(classifierResult != datingLabels[i]):
