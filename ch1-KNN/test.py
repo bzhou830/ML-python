f414b7e43c7d1108f3f74a01e07e293de810ffbc
robin <chou_robin@hotmail.com>
2016-06-29 16:21:58 +0800

    add

---
+++ ch1-KNN/test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding=utf-8
+import KNN
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+datingDataMat,datingLabels = KNN.file2matrix('datingTestSet2.txt')
+
+print datingDataMat,datingLabels
+
+fig=plt.figure()
+ax=fig.add_subplot(111)
+ax.scatter(datingDataMat[:,1],datingDataMat[:,2],
+          15.0 * np.array(datingLabels), 15.0 * np.array(datingLabels))
+plt.show()
+
