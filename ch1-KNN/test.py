2ecbc4fa311b56259516f894c888213ed0e8671e
robin <chou_robin@hotmail.com>
2016-07-01 16:23:56 +0800

    modify

--- ch1-KNN/test.py
+++ ch1-KNN/test.py
@@ -17,4 +17,21 @@ ax.scatter(datingDataMat[:,1],datingDataMat[:,2],
 plt.show()
 '''
 
-KNN.datingClassTest()
+#KNN.datingClassTest()
+
+def classifyPerson():
+    print "输入相关信息"
+    resultList = ['一点不喜欢','有点希望','可能性很大']
+    percentTats = float(raw_input("玩游戏时间数目?"))
+    ffMiles = float(raw_input("旅游公路数?"))
+    ice = float(raw_input("冰淇淋消耗量?"))
+    datingDataMat,datingLabels = KNN.file2matrix('datingTestSet2.txt')
+    normMat,ranges,minVals = KNN.autoNorm(datingDataMat)
+    inArr = np.array([ffMiles,percentTats,ice])
+    classfierRt = KNN.classify0((inArr-minVals)/ranges,normMat,datingLabels,3)
+    print resultList[classfierRt - 1]
+
+
+classifyPerson()
+
+    
