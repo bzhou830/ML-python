9e054acf04cb1ae2a66a2093b379a6b1da865de2
robin <chou_robin@hotmail.com>
2016-07-06 16:07:19 +0800

    modify

--- ch2-dicision_tree/trees.py
+++ ch2-dicision_tree/trees.py
@@ -6,20 +6,23 @@ from math import log
 
 #计算香农熵
 def calcShannonEnt(dataSet):
-    numEntries = len(dataSet)
-    labelCounts = {}
-    for featVec in dataSet:
+    numEntries = len(dataSet)                           #数据集合中的数据条数
+    labelCounts = {}                                    #数据的标签数
+    
+    for featVec in dataSet:                             #遍历数据集合
         currentLabel = featVec[-1]
         if currentLabel not in labelCounts.keys():
             labelCounts[currentLabel] = 0
         labelCounts[currentLabel] += 1
-        shannonEnt = 0.0
-        for key in labelCounts:
-            prob = float(labelCounts[key])/numEntries
-            shannonEnt -= prob * log(prob, 2)
+    
+    shannonEnt = 0.0
+
+    for key in labelCounts:                             #计算香农熵
+        prob = float(labelCounts[key])/numEntries
+        shannonEnt -= prob * log(prob, 2)
     return shannonEnt
 
-
+#创建数据集合
 def createDataSet():
     dataSet = [[1,1,'yes'],
               [1,1,'yes'],
@@ -29,12 +32,14 @@ def createDataSet():
     labels = ['no surfacing','flippers']
     return dataSet,labels
 
-myDat,labels = createDataSet()
-shnnon  = calcShannonEnt(myDat)
-print shnnon
+#测试香农熵计算函数
+def test():
+    myDat,labels = createDataSet()      #创建一个数据集
+    shnnon  = calcShannonEnt(myDat)     #计算数据集合的香农熵
+    print shnnon                        #输出显示计算结果
 
 
-
+test()
 
 
 
